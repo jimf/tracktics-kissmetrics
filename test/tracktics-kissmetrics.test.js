7afcdf5d72149fff08032730776844ef6ebd2e23
Jim Fitzpatrick <fitzpatrick.jim@gmail.com>
2015-06-17 15:55:09 -0400

    lib: Initial implementation

---
+++ test/tracktics-kissmetrics.test.js
@@ -0,0 +1,78 @@
+'use strict';
+
+var trackticsKiss = require('..');
+
+function resetKmq() {
+    global.window._kmq = [];
+}
+
+describe('Tracktics KISSmetrics Plugin', function() {
+    var subject;
+
+    beforeEach(function() {
+        subject = trackticsKiss();
+    });
+
+    describe('#trackPage()', function() {
+        var url;
+
+        beforeEach(function() {
+            url = '/some/url';
+            resetKmq();
+            subject.trackPage(url);
+        });
+
+        it('should push expected data to _kmq queue', function() {
+            expect(global.window._kmq[0]).toEqual(['record', 'Pageview', { Page: url }]);
+        });
+    });
+
+    describe('#trackEvent()', function() {
+        var action, properties;
+
+        beforeEach(function() {
+            action = 'click';
+            properties = { event: 'Download' };
+        });
+
+        describe('when no additional properties are specified', function() {
+
+            beforeEach(function() {
+                resetKmq();
+                subject.trackEvent(action, properties);
+            });
+
+            it('should push expected data to _kmq queue', function() {
+                expect(global.window._kmq[0]).toEqual(['record', properties.event]);
+            });
+        });
+
+        describe('when additional properties are specified', function() {
+
+            beforeEach(function() {
+                resetKmq();
+                properties.plan = 'Pro';
+                subject.trackEvent(action, properties);
+            });
+
+            it('should push expected data to _kmq queue', function() {
+                expect(global.window._kmq[0]).toEqual(['record', properties.event, { plan: 'Pro' }]);
+            });
+        });
+    });
+
+    describe('when window._kmq is already defined', function() {
+        var kmq;
+
+        beforeEach(function() {
+            kmq = [];
+            global.window._kmq = kmq;
+
+            trackticsKiss();
+        });
+
+        it('should preserve window._kmq', function() {
+            expect(global.window._kmq).toBe(kmq);
+        });
+    });
+});
